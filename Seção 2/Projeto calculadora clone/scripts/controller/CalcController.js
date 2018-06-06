4ada3fa11e34675856fc98a0d63f5dc5a313f798
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-06-06 13:26:11 -0300

    Projeto calculadora Seção 2 Aula 8

--- Seção 2/Projeto calculadora clone/scripts/controller/CalcController.js
+++ Seção 2/Projeto calculadora clone/scripts/controller/CalcController.js
@@ -1,3 +1,22 @@
 class CalcController {
+    constructor(){
+        this._displayCalc = "0";
+        this._dataAtual;
+    }
+
+    get displayCalc() {
+        return this._displayCalc;
+    }
     
+    get dataAtual() {
+        return this._dataAtual;
+    }
+    
+    set displayCalc(novoValor) {
+        this._displayCalc = novoValor;
+    }
+
+    set dataAtual(novoValor) {
+        this._dataAtual = novoValor;
+    }  
 }
