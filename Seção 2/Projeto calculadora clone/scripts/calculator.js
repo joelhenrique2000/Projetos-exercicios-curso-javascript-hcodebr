1a10fa4389435d2425c553adfbb8b43c4f78561c
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-06-06 22:19:16 -0300

    Projeto calculadora Seção 2 - Aula 10

--- Seção 2/Projeto calculadora clone/scripts/calculator.js
+++ Seção 2/Projeto calculadora clone/scripts/calculator.js
@@ -1,1 +1,4 @@
-let calculator = new CalcController();
+window.calculator = new CalcController();
+
+calculator.displayDate = new Date().toLocaleDateString('pt-BR');
+calculator.displayTime = new Date().toLocaleTimeString('pt-BR')
--- Seção 2/Projeto calculadora clone/scripts/controller/CalcController.js
+++ Seção 2/Projeto calculadora clone/scripts/controller/CalcController.js
@@ -1,30 +1,52 @@
 class CalcController {
     constructor(){
-        this._displayCalc = "0";
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
     }
 
     initialize(){
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        this.setDisplayDateTime();
 
-        displayCalcEl.innerHTML = "4567";
-        dateEl.innerHTML = "00/00/0000";
-        timeEl.innerHTML = "00:00:00";
+        setInterval(() => {
+            this.setDisplayDateTime();
+        },1000); 
+    }
+
+    setDisplayDateTime() {
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale,{day: "2-digit",month: "long", year: "numeric"});
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(newValue) {
+        this._timeEl.innerHTML = newValue;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+
+    set displayDate(newValue) {
+        this._dateEl.innerHTML = newValue;
     }
         
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
+    }
+
+    set displayCalc(newValue) {
+        this._displayCalcEl.innerHTML = newValue;
     }
     
     get currentDate() {
-        return this._currentDate;
-    }
-    
-    set displayCalc(newValue) {
-        this._displayCalc = newValue;
+        return new Date();
     }
 
     set currentDate(newValue) {
