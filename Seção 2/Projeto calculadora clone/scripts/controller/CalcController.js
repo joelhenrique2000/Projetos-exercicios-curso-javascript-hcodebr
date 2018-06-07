c325746b5e1b7200a93d21f80eb008584670e057
joelhenrique2000 <joelhenrique2000@gmail.com>
2018-06-06 22:28:31 -0300

    Projeto calculadora seção 2 - aula 11

--- Seção 2/Projeto calculadora clone/scripts/controller/CalcController.js
+++ Seção 2/Projeto calculadora clone/scripts/controller/CalcController.js
@@ -16,6 +16,10 @@ class CalcController {
         },1000); 
     }
 
+    initButtonsEvents() {
+        document.querySelectorAll("#buttons > g, #parts > g");
+    }
+
     setDisplayDateTime() {
         this.displayDate = this.currentDate.toLocaleDateString(this._locale,{day: "2-digit",month: "long", year: "numeric"});
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
